7e16ebde1148456e67d6e0d14c2637b970058bb0
jixiang52002 <37310534@qq.com>
2016-08-17 10:49:14 +0800

    1.效果展示
    2.增加删除增添动画，以及分割线

--- app/src/main/java/com/jixiang52002/recycleviewdemo/MainActivity.java
+++ app/src/main/java/com/jixiang52002/recycleviewdemo/MainActivity.java
@@ -2,6 +2,7 @@ package com.jixiang52002.recycleviewdemo;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.DefaultItemAnimator;
 import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
@@ -15,9 +16,13 @@ public class MainActivity extends AppCompatActivity {
         rv=new RecyclerView(this);
         setContentView(rv);
         //设置recycleview的布局对象,死党额惨呼为是否反转
-//        rv.setLayoutManager(new LinearLayoutManager(this,LinearLayoutManager.HORIZONTAL,true));
-        rv.setLayoutManager(new GridLayoutManager(this,2));
+        rv.setLayoutManager(new LinearLayoutManager(this,LinearLayoutManager.HORIZONTAL,false));
+//        rv.setLayoutManager(new C(this,2));
 
+        //设置Item增加、移除动画
+        rv.setItemAnimator(new DefaultItemAnimator());
+//添加分割线
+        rv.addItemDecoration(new DividerItemDecoration(this,DividerItemDecoration.HORIZONTAL_LIST));
 
         //为recycleview设置适配器
         rv.setAdapter(new MyAdapter());
